eb579183cfc413d72e2ae7143c9b0c31300e7092
adindadiwhr <adinda.dwi2803@gmail.com>
2023-09-25 10:27:25 +0700

    Base Project Ionic Leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,54 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map!: L.Map;
 
+  constructor() { }
+  // ngOnInit() {
+
+  // }
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([-7.774687474872216, 110.37452193584669], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    const markerIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+    });
+
+
+    const baseMaps = {
+      'OpenStreetMap': L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }),
+      'Satellite': L.tileLayer('https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution: '&copy; <a href="https://maps.google.com">Google Maps</a>'
+      }),
+      'Terrain': L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17,
+        attribution: '&copy; <a href="https://www.opentopomap.org">OpenTopoMap</a> contributors'
+      })
+    };
+
+    // Tambahkan kontrol lapisan ke peta
+    L.control.layers(baseMaps).addTo(this.map);
+
+    const marker = L.marker([-7.774687474872216, 110.37452193584669], { icon: markerIcon }).addTo(this.map);
+    // Tambahkan popup ke marker
+    marker.bindPopup('<b>Lokasi</b><br>Sekolah Vokasi UGM').openPopup();
+    
+  }
 }
+
+
+
+
